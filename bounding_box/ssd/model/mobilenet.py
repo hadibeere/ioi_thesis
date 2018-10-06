08d93904251a4d5de4a9c33056f56b42b0f797a3
hadibeere <31854160+hadibeere@users.noreply.github.com>
2018-10-06 03:09:58 +0200

    Corrected number of input channels
    Mobilenet input channels did not match the actual number of channels.

--- bounding_box/ssd/model/mobilenet.py
+++ bounding_box/ssd/model/mobilenet.py
@@ -27,7 +27,7 @@ class MobileNetV1(nn.Module):
             )
 
         self.model = nn.Sequential(
-            conv_bn(3, 32, 2),
+            conv_bn(2, 32, 2),
             conv_dw(32, 64, 1),
             conv_dw(64, 128, 2),
             conv_dw(128, 128, 1),
