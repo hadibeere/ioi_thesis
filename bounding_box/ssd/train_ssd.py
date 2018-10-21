174f4c39535e18fc55c9ad460f23ee2b5b79df6a
hadibeere <31854160+hadibeere@users.noreply.github.com>
2018-10-21 13:59:15 +0200

    Save Model DataParallel
    If we use Dataparallel with multiple GPUs the syntax for saving the
    model is different. Added switch to choose correct variant.

--- bounding_box/ssd/train_ssd.py
+++ bounding_box/ssd/train_ssd.py
@@ -283,5 +283,8 @@ if __name__ == '__main__':
                 f"Validation Classification Loss: {val_classification_loss:.4f}"
             )
             model_path = os.path.join(args.checkpoint_folder, f"ssd-Epoch-{epoch}-Loss-{val_loss}.pth")
-            net.save(model_path)
+            if torch.cuda.device_count() > 1:
+                torch.save(net.module.state_dict(), model_path)
+            else:
+                net.save(model_path)
             logging.info(f"Saved model {model_path}")
