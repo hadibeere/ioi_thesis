47b7436cbf48f13d10577074d7f116f3ec6a97a7
hadibeere <31854160+hadibeere@users.noreply.github.com>
2018-10-30 03:29:10 +0100

    Changed priors
    Allowed smaller priors for smallest feature map size.
    
    This will make sure to get a better start fit for prior matching.

--- bounding_box/ssd/config/mobilenetv1_ssd_config.py
+++ bounding_box/ssd/config/mobilenetv1_ssd_config.py
@@ -6,20 +6,19 @@ from ssd.utils.box_utils import SSDSpec, SSDBoxSizes, generate_ssd_priors
 image_size = 300
 mean = 583.6798
 std = 70.0333
-image_mean = [mean, mean]
-image_std = [std, std]
+image_mean = np.array([mean, mean])
+image_std = std
 iou_threshold = 0.45
 center_variance = 0.1
 size_variance = 0.2
 
 specs = [
-    SSDSpec(19, 16, SSDBoxSizes(60, 105), [2, 3]),
-    SSDSpec(10, 32, SSDBoxSizes(105, 150), [2, 3]),
-    SSDSpec(5, 64, SSDBoxSizes(150, 195), [2, 3]),
-    SSDSpec(3, 100, SSDBoxSizes(195, 240), [2, 3]),
-    SSDSpec(2, 150, SSDBoxSizes(240, 285), [2, 3]),
-    SSDSpec(1, 300, SSDBoxSizes(285, 330), [2, 3])
+    SSDSpec(19, 16, SSDBoxSizes(20, 65), [2, 3]),
+    SSDSpec(10, 32, SSDBoxSizes(65, 110), [2, 3]),
+    SSDSpec(5, 64, SSDBoxSizes(110, 155), [2, 3]),
+    SSDSpec(3, 100, SSDBoxSizes(155, 200), [2, 3]),
+    SSDSpec(2, 150, SSDBoxSizes(200, 245), [2, 3]),
+    SSDSpec(1, 300, SSDBoxSizes(245, 290), [2, 3])
 ]
-
 
 priors = generate_ssd_priors(specs, image_size)
