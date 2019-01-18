1cb3ac428790c65b230c7f663ad7e563c6dd9152
hadibeere <hadibeere@kjux>
2019-01-18 08:42:50 +0100

    Bugfix YOLO train
    Corrected some typos

--- bounding_box/yolo/train_yolo.py
+++ bounding_box/yolo/train_yolo.py
@@ -25,7 +25,7 @@ parser.add_argument('--border', default=20, type=float,
                          'the others it\'s the percentage of the distance between tips e.g 0.5 for 50% of the distance )')
 parser.add_argument('-epochs', type=int, default=100, help='number of epochs')
 parser.add_argument('-cfg', type=str, default='cfg/yolov3.cfg', help='cfg file path')
-parser.add_argument("--weights", dest = 'weightsfile', help ="weightsfile", type = str)
+parser.add_argument("--weights", dest='weightsfile', help ="weightsfile", type = str)
 parser.add_argument('-resume', default=False, help='resume training flag')
 parser.add_argument('--log', help='Path of log file. If log is set the logging will be activated else '
                                   'nothing will be logged.')
@@ -44,10 +44,10 @@ device = torch.device('cuda' if cuda else 'cpu')
 rd_seed = 456
 random.seed(rd_seed)
 np.random.seed(rd_seed)
-torch.manual_seed()
+torch.manual_seed(rd_seed)
 if cuda:
-    torch.cuda.manual_seed(0)
-    torch.cuda.manual_seed_all(0)
+    torch.cuda.manual_seed(rd_seed)
+    torch.cuda.manual_seed_all(rd_seed)
 
 
 def train(loader, model, optimizer, criterion, device):
@@ -58,7 +58,7 @@ def train(loader, model, optimizer, criterion, device):
     anchors = net_info['anchors']
     num_anchors = net_info['num_anchors']
     det_strides = [32, 16, 8]
-    metrics = StatCollectorTrain(416, anchors, det_strides=det_strides, conf_threshold=0.5, nms_threshold=0.0,
+    metrics = StatCollectorTrain(416, anchors, det_strides=det_strides, conf_threshold=0.5, nms_threshold=0.1,
                                  iou_threshold=0.5, device=device)
     for _, data in enumerate(loader):
         optimizer.zero_grad()
@@ -82,7 +82,9 @@ def test(loader, model, criterion, device):
     net_info = model.net_info
     anchors = net_info['anchors']
     num_anchors = net_info['num_anchors']
-    metrics = StatCollectorTrain(416, anchors, conf_threshold=0.5, nms_threshold=0.0, iou_threshold=0.5, device=device)
+    det_strides = [32, 16, 8]
+    metrics = StatCollectorTrain(416, anchors, det_strides=det_strides, conf_threshold=0.5, nms_threshold=0.1,
+                                 iou_threshold=0.5, device=device)
     for _, data in enumerate(loader):
         with torch.no_grad():
             output = model(data[0].to(device), is_test=True)
@@ -106,7 +108,7 @@ if __name__ == '__main__':
 
     # Initialize model
     model = Darknet(args.cfg)
-    if args.weightfile:
+    if args.weightsfile:
         model.load_weights(args.weightsfile, stop=74)
         logging.info("Loaded original darknet weights.")
 
@@ -116,16 +118,18 @@ if __name__ == '__main__':
 
     net_options = model.net_info
 
+    num_workers=4
+    image_size=int(net_options['width'])
     ##Parse the config file
-    batch_size = net_options['batch']
-    num_input = net_options['channels']
-    momentum = net_options['momentum']
-    decay = net_options['decay']  # Penalty for regularisation
-    learning_rate = net_options['learning_rate']  # Initial learning rate
+    batch_size = int(net_options['batch'])
+    num_input = int(net_options['channels'])
+    momentum = float(net_options['momentum'])
+    decay = float(net_options['decay'])  # Penalty for regularisation
+    learning_rate = float(net_options['learning_rate'])  # Initial learning rate
 
-    train_transform = pr.TrainAugmentation(args.image_size, p=0.5)
-    test_transform = pr.TestTransform(args.image_size)
-    target_transform = pr.MatchAnchors(net_options['anchors'], net_options['num_anchors'], inp_dim=net_options['width'],
+    train_transform = pr.TrainAugmentation(image_size, p=0.5)
+    test_transform = pr.TestTransform(image_size)
+    target_transform = pr.MatchAnchors(net_options['anchors'], net_options['num_anchors'], inp_dim=image_size,
                                        strides=[32, 16, 8], num_classes=1)
     logging.info("Prepare training dataset.")
     if args.gt == 'area_tips':
@@ -148,13 +152,13 @@ if __name__ == '__main__':
                                       border=args.border, num_channels=num_input, transform=test_transform,
                                       target_transform=target_transform)
 
-    train_loader = torch.utils.data.DataLoader(train_dataset, args.batch_size, num_workers=args.num_workers, shuffle=True)
-    val_loader = torch.utils.data.DataLoader(val_dataset, args.batch_size, num_workers=args.num_workers, shuffle=False)
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size, num_workers=num_workers, shuffle=True)
+    val_loader = torch.utils.data.DataLoader(val_dataset, batch_size, num_workers=num_workers, shuffle=False)
 
-    criterion = ls.YOLOLoss(delta_coord=net_options['delta_coord'], delta_obj=net_options['delta_obj'],
-                            delta_noobj=net_options['delta_noobj'], delta_cls=net_options['delta_cls'])
+    criterion = ls.YOLOLoss(delta_coord=float(net_options['delta_coord']), delta_obj=float(net_options['delta_obj']),
+                            delta_noobj=float(net_options['delta_noobj']), delta_cls=float(net_options['delta_cls']))
     if args.use_focal_loss:
-        criterion = fl.FocalLoss(gamma=net_options['gamma'], alpha=net_options['alpha'])
+        criterion = fl.FocalLoss(gamma=float(net_options['gamma']), alpha=float(net_options['alpha']))
 
     # Reload saved optimizer state
     start_epoch = 0
@@ -165,13 +169,12 @@ if __name__ == '__main__':
     optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum, weight_decay=decay)
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer=optimizer)
     chpts = SavePointManager(args.checkpoint_folder, args.max_chpt)
-    for epoch in range(args.num_epochs):
-        scheduler.step()
-        logging.info(
-            f"Epoch: {epoch}, " +
-            f"learning rate: {scheduler.get_lr()[0]}, "
-        )
-        loss, precision, recall = train(train_loader, model, criterion, optimizer)
+    for epoch in range(args.epochs):
+        #logging.info(
+        #    f"Epoch: {epoch}, " +
+        #    f"learning rate: {scheduler.get_lr()[0]}, "
+        #)
+        loss, precision, recall = train(train_loader, model, optimizer, criterion, device)
 
         logging.info(
             f"Epoch: {epoch}, " +
@@ -188,6 +191,7 @@ if __name__ == '__main__':
             f"Validation Recall: {recall:.3f}"
         )
         model.train()
+        scheduler.step(val_loss)
         if torch.cuda.device_count() > 1:
             state = {'epoch': epoch, 'state_dict': model.module.state_dict(),
                      'optimizer': optimizer.state_dict(), 'scheduler': scheduler.state_dict()}
